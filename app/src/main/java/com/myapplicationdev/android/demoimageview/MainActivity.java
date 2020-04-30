cb6cea6cc5b7cfcce8fd06e4b1854c61bb1a0ac8
18045945 <aungmoethet2000@gmail.com>
2020-04-30 10:35:28 +0800

    Initial commit

---
+++ app/src/main/java/com/myapplicationdev/android/demoimageview/MainActivity.java
@@ -0,0 +1,21 @@
+package com.myapplicationdev.android.demoimageview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageView imageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        imageView = findViewById(R.id.imageView2);
+
+        imageView.setImageResource(R.drawable.day2);
+    }
+}
